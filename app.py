66d5888a7172011ba9e0ce0fe3b000e0ddb4a89d
msrodrigues105 <47826691+msrodrigues105@users.noreply.github.com>
2022-09-09 10:10:42 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,96 @@
+import os
+import uuid
+import flask
+import urllib
+from PIL import Image
+from tensorflow.keras.models import load_model
+from flask import Flask , render_template  , request , send_file
+from tensorflow.keras.preprocessing.image import load_img , img_to_array
+
+app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model = load_model(os.path.join(BASE_DIR , 'model.h5'))
+
+
+ALLOWED_EXT = set(['jpg' , 'jpeg' , 'png' , 'jfif'])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXT
+
+#classes ={0: 'apple', 1: 'banana', 2: 'beetroot', 3: 'bell pepper', 4: 'cabbage', 5: 'capsicum', 6: 'carrot', 7: 'cauliflower', 8: 'chilli pepper', 9: 'corn', 10: 'cucumber', 11: 'eggplant', 12: 'garlic', 13: 'ginger', 14: 'grapes', 15: 'jalepeno', 16: 'kiwi', 17: 'lemon', 18: 'lettuce',
+          #19: 'mango', 20: 'onion', 21: 'orange', 22: 'paprika', 23: 'pear', 24: 'peas', 25: 'pineapple', 26: 'pomegranate', 27: 'potato', 28: 'raddish', 29: 'soy beans', 30: 'spinach', 31: 'sweetcorn', 32: 'sweetpotato', 33: 'tomato', 34: 'turnip', 35: 'watermelon'}
+
+classes = ['apple' ,'banana', 'beetroot' , 'bell pepper' , 'cabbage' ,'capsicum' ,'carrot', 'cauliflower' ,'chilli pepper' ,'corn', 'cucumber', 'eggplant', 'garlic', 'ginger', 'grapes', 'jalepeno', 'kiwi', 'lemon', 'lettuce', 
+           'mango', 'onion', 'orange', 'paprika', 'pear', 'peas', 'pineapple', 'pomegranate', 'potato', 'raddish', 'soy beans', 'spinach', 'sweetcorn', 'sweetpotato', 'tomato', 'turnip', 'watermelon']
+
+
+def predict(filename , model):
+    img = load_img(filename , target_size = (128 , 128))
+    img = img_to_array(img)
+    img = img.reshape(1 , 128 ,128 ,3)
+
+    #img = img.astype('float32')
+    #img = img/255.0
+    result = model.predict(img)
+
+    dict_result = {}
+    for i in range(35):   #mduar para 35
+        dict_result[result[0][i]] = classes[i]
+
+    res = result[0]
+    res.sort()
+    res = res[::-1]
+    prob = res[:3]
+    
+    prob_result = []
+    class_result = []
+    for i in range(3):
+        prob_result.append((prob[i]*100).round(2))
+        class_result.append(dict_result[prob[i]])
+
+    return class_result , prob_result
+    return result
+
+
+@app.route('/')
+def home():
+        return render_template("index.html")
+
+@app.route('/success' , methods = ['GET' , 'POST'])
+def success():
+    error = ''
+    target_img = os.path.join(os.getcwd() , 'static/images')
+    if request.method == 'POST':
+        if (request.files):
+            file = request.files['file']
+            if file and allowed_file(file.filename):
+                file.save(os.path.join(target_img , file.filename))
+                img_path = os.path.join(target_img , file.filename)
+                img = file.filename
+                
+                class_result , prob_result = predict(img_path , model)
+                #result = predict(img_path , model)
+               # pred_class = result.argmax(axis=-1)
+                
+                predictions = {
+                      "class1":class_result[0],
+                      #"class1":pred_class,
+
+                        
+                }
+
+            else:
+                error = "Please upload images of jpg , jpeg and png extension only"
+
+            if(len(error) == 0):
+                return  render_template('success.html' , img  = img , predictions = predictions)
+            else:
+                return render_template('index.html' , error = error)
+
+    else:
+        return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
+
